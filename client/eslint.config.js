c79753831ab5e78690c5676fb368d3db1be87f39
Jessica Bruner <info@codeherllc.com>
2026-03-13 13:51:08 -0700

    Sync working VPS hotfixes and email submission updates

--- client/eslint.config.js
+++ client/eslint.config.js
@@ -1,29 +1,15 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import { defineConfig, globalIgnores } from 'eslint/config'
-
-export default defineConfig([
-  globalIgnores(['dist']),
-  {
-    files: ['**/*.{js,jsx}'],
-    extends: [
-      js.configs.recommended,
-      reactHooks.configs.flat.recommended,
-      reactRefresh.configs.vite,
-    ],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-      parserOptions: {
-        ecmaVersion: 'latest',
-        ecmaFeatures: { jsx: true },
-        sourceType: 'module',
-      },
-    },
-    rules: {
-      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
-    },
-  },
-])
+module.exports = {
+  apps: [
+    {
+      name: "codeher-api",
+      script: "./src/server.js",
+      cwd: "/var/www/codeher-v2/server",
+      instances: 1,
+      exec_mode: "fork",
+      env: {
+        NODE_ENV: "production",
+        PORT: 5000
+      }
+    }
+  ]
+};
