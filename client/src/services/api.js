56c5fcb2ea7aff73f1ab61bc603643042237d29b
Jessica Bruner <info@codeherllc.com>
2026-03-13 15:57:43 -0700

    update

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,8 +1,10 @@
 import axios from "axios";
 
-const api = axios.create({
+const API = axios.create({
   baseURL: import.meta.env.VITE_API_URL || "/api",
-  withCredentials: true
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
-export default api;
+export default API;
